9c006f445428cde582f00d91505da52497950190
pitambarsahoo86 <pitambar.bs@gmail.com>
2024-10-21 21:18:16 +0530

    Learnt how to use 'className' in react.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter , Link , Route , Routes} from 'react-router-dom'
+import './index.css';
+import {BrowserRouter , Link ,NavLink, Route , Routes} from 'react-router-dom'
 import Employee from './Component/Employee'
 import Project from './Component/Project'
 import Department from './Component/Department'
 import NotFound from './notfound';
+
 
 // function App(){
 //   return <div>
@@ -28,9 +30,9 @@ function App(){
   return <div>
     <h2>App Component</h2>
     <ul>
-      <li><Link to='/Employee'>Employee</Link></li>
-      <li><Link to='Project'>Project</Link></li>
-      <li><Link to='Department'>Department</Link></li>
+      <li><NavLink to='/Employee' className={({isActive})=>(isActive?'handleNavLink':undefined)}>Employee</NavLink></li>
+      <li><NavLink to='Project' className={({isActive})=>(isActive?'handleNavLink':undefined)}>Project</NavLink></li>
+      <li><NavLink to='Department' className={({isActive})=>(isActive?'handleNavLink':undefined)}>Department</NavLink></li>
     </ul>
     <Routes>
       <Route path='/' element={<Employee/>}></Route> {/*In latest verson there is no use of components instead we are now using 'element'..*/}
